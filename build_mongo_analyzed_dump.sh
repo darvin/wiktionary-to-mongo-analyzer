dae929ef5f14d6f0fd77d283858afcf0c2831f78
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-10-11 17:02:21 -0700

    importing

--- build_mongo_analyzed_dump.sh
+++ build_mongo_analyzed_dump.sh
@@ -2,4 +2,12 @@
 
 mkdir ./build
 
-wget `./scripts/mongo_dump_circleci_latest_url.js` -P ./build
+BZIPPED_DUMP=./build/enWiktionaryDump.json.bz2
+DUMP=enWiktionaryDump.json
+if [ ! -f $BZIPPED_DUMP ]; then
+	wget `./scripts/mongo_dump_circleci_latest_url.js` -P ./build
+fi
+
+bunzip2 $BZIPPED_DUMP
+
+mongoimport  --db wiktionaryToMongo $DUMP
