784993bb9c2a0ad366f3718e555c123a636f4ea5
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-10-11 17:05:12 -0700

    fixed script

--- build_mongo_analyzed_dump.sh
+++ build_mongo_analyzed_dump.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -e
 
 mkdir ./build
 
 BZIPPED_DUMP=./build/enWiktionaryDump.json.bz2
-DUMP=enWiktionaryDump.json
+DUMP=./build/enWiktionaryDump.json
 if [ ! -f $BZIPPED_DUMP ]; then
 	wget `./scripts/mongo_dump_circleci_latest_url.js` -P ./build
 fi
@@ -11,3 +12,5 @@ fi
 bunzip2 $BZIPPED_DUMP
 
 mongoimport  --db wiktionaryToMongo $DUMP
+
+rm $DUMP
