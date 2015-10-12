629b677f67cb6237d96cbd491913bff212e76af2
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-10-11 18:58:41 -0700

    analyze in series

--- build_mongo_analyzed_dump.sh
+++ build_mongo_analyzed_dump.sh
@@ -5,6 +5,8 @@ set -e
 
 BZIPPED_DUMP=./build/enWiktionaryDump.json.bz2
 DUMP=./build/enWiktionaryDump.json
+MONGO_DUMP=$CIRCLE_ARTIFACTS/enWiktionary.json.bz2
+
 if [ ! -f $BZIPPED_DUMP ]; then
 	mkdir ./build || true
 	wget `./scripts/mongo_dump_circleci_latest_url.js` -P ./build
@@ -15,3 +17,8 @@ bunzip2 $BZIPPED_DUMP
 mongoimport  --db wiktionaryToMongo --collection enWiktionaryDump --drop --file  $DUMP
 
 rm $DUMP
+
+
+./scripts/analyze.js
+
+mongoexport --db wiktionaryToMongo --collection enWiktionary |bzip2 > $MONGO_DUMP
--- scripts/analyze.js
+++ scripts/analyze.js
@@ -4,7 +4,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/wiktionaryToMongo';
 var wiktAnalyzer = require('enwiktionary-analyzer');
-var LOG_EVERY = 100;
+var LOG_EVERY = 10000;
 MongoClient.connect(url, function(err, db) {
 
   var col = db.collection('enWiktionaryDump');
@@ -15,42 +15,53 @@ MongoClient.connect(url, function(err, db) {
     var index = 0;
     var indexSaved = 0;
     cursor.count(function(err, total){
-      cursor.on('data', function(doc) {
-        index ++;
-        if (index % LOG_EVERY==0) {
-          console.log ("Read: "+index+"/"+total);
-        }
-        
-        // console.log(" - "+doc.title+ " NS: "+doc.namespace);
 
+      var analyzeAndWrite = function(doc, callback) {
         wiktAnalyzer.analyzer.parseArticle(doc.title, doc.text, function(err, parsedArticle) {
           indexSaved++;
           if (indexSaved % LOG_EVERY==0) {
-            console.log ("Analyzed: "+indexSaved+"/"+total);
+            console.log ("Analyzed: "+indexSaved+"/"+total + "(current: '"+doc.title+"' " +Object.keys(parsedArticle)+")");
           }
           if (err || !parsedArticle || Object.keys(parsedArticle).length == 0) {
             console.error("Error parsing article: '"+doc.title+"' ", err)
+            callback(err);
           } else {
-
+            var outputIndex = 0;
             Object.keys(parsedArticle).forEach(function(lang) {
               var word = parsedArticle[lang];
               word.wordLanguage = word.word[0];
               word.wordName = word.word[1];
               colOutput.insert(word, function(err, r){
-                
+                outputIndex ++;
+                if (outputIndex==Object.keys(parsedArticle).length) {
+                  callback(null);
+                }
 
               });
 
             });
           }
         });
+      };
 
+      var analyzeNext = function() {
         
-      });
-
-      cursor.once('end', function() {
-        db.close();
-      });
+        cursor.nextObject(function(err, doc) {
+          if (err)
+            console.error(err);
+          if (!doc) {
+            db.close();
+          }
+          index ++;
+          if (index % LOG_EVERY==0) {
+            console.log ("Read: "+index+"/"+total + " (current: '"+doc.title+"' )");
+          }
+          analyzeAndWrite(doc, function(err) {
+            analyzeNext();
+          })
+        });
+      }
+      analyzeNext();
 
     });
   });
