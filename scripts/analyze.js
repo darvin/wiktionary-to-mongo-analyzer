3025c291dd4dcc0c75f6e33c5a69a8f46be25cfe
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-10-11 23:13:49 -0700

    time logging

--- scripts/analyze.js
+++ scripts/analyze.js
@@ -4,7 +4,10 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/wiktionaryToMongo';
 var wiktAnalyzer = require('enwiktionary-analyzer');
-var LOG_EVERY = 300;
+var LOG_EVERY = 1;
+var LIMIT = 50;
+
+console.log("FOUND CPUS: ",require('os').cpus().length);
 MongoClient.connect(url, function(err, db) {
 
   var col = db.collection('enWiktionaryDump');
@@ -15,10 +18,15 @@ MongoClient.connect(url, function(err, db) {
     var index = 0;
     var indexSaved = 0;
     cursor.count(function(err, total){
+      console.time("total");
 
       var analyzeAndWrite = function(doc, callback) {
         wiktAnalyzer.analyzer.parseArticle(doc.title, doc.text, function(err, parsedArticle) {
           indexSaved++;
+          if (indexSaved>=LIMIT) {
+            console.timeEnd("total");
+            console.log("FINISHED ANALYZING");
+          }
           if (indexSaved % LOG_EVERY==0) {
             console.log ("Analyzed: "+indexSaved+"/"+total + "(current: '"+doc.title+"' " +Object.keys(parsedArticle)+")");
           }
@@ -45,20 +53,32 @@ MongoClient.connect(url, function(err, db) {
       };
 
       var analyzeNext = function() {
-        
+        var stopped = false;
+        var stop = function() {
+          if (stopped)
+            return;
+          else {
+            console.log("FINISHED READING");
+
+            stopped = true;
+            db.close();
+          }
+        }
         cursor.nextObject(function(err, doc) {
           if (err)
             console.error(err);
-          if (!doc) {
-            db.close();
+          if (!doc || (LIMIT && index>=LIMIT)) {
+            stop();
+          } else {
+            index ++;
+            if (index % LOG_EVERY==0) {
+              console.log ("Read: "+index+"/"+total + " (current: '"+doc.title+"' )");
+            }
+            analyzeAndWrite(doc, function(err) {
+              analyzeNext();
+            })
+
           }
-          index ++;
-          if (index % LOG_EVERY==0) {
-            console.log ("Read: "+index+"/"+total + " (current: '"+doc.title+"' )");
-          }
-          analyzeAndWrite(doc, function(err) {
-            analyzeNext();
-          })
         });
       }
       analyzeNext();
